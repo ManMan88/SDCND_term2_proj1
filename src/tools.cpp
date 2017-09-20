38df4adbb25e3d0be610a7980a4607f45800deb2
Ron Danon <rondanon@gmail.com>
2017-09-20 15:19:39 +0300

    add RMSE and Jacobian functions implementation

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "tools.h"
+#include <cmath>
 
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
@@ -15,6 +16,19 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd residual(4);
+  for(int i=0; i < estimations.size(); ++i){
+    VectorXd err = estimations[i] - ground_truth[i];
+    residual = err.array()*err.array();
+    rmse += residual;
+  }
+
+  //calculate the mean
+  rmse = rmse/estimations.size();
+  //calculate the squared root
+  rmse = mean_residuals.array().sqrt();
+  
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -22,4 +36,31 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+  // initialize the Jacobian Matrix
+  MatrixXd Hj(3,4);
+  //recover state parameters
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+  
+  //check division by zero
+  if (abs(px) < 0.00001 && abs(py) < 0.00001) {
+	// if both px and py are 0, return a 0 matrix
+	// in such a case, the state estimation will be the prediction, and the P (uncertainty) will stay the same
+    cout << "NOTE - px and py are both 0! Returning a 0 matrix" << endl;
+	Hj = MatrixXd(3,4);
+  }
+  else {
+    //compute the Jacobian matrix
+    float px2py2 = px*px+py*py;
+	float s_px2py2 = sqrt(px2py2);
+	float px2py2_32 = px2py2*s_px2py2;
+	float vp = vx*py-vy*px;
+	
+	Hj << px/s_px2py2, py/s_px2py2, 0, 0,
+	    -py/px2py2, px/px2py2, 0, 0,
+	    py*vp/px2py2_32, px*vp/px2py2_32, px/s_px2py2, py/s_px2py2;
+  }
+  return Hj
 }
